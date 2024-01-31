9be93233bc3629ed88d67aba9a0bcffcc350fef0
Morris, Kevin <Kevin.Morris@noblis.org>
2024-01-31 16:06:29 -0500

    permit cross origin

--- src/main/java/com/pocketbook/pocketbookapi/controller/ItemController.java
+++ src/main/java/com/pocketbook/pocketbookapi/controller/ItemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.pocketbook.pocketbookapi.service.ItemService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Tag(name = "Item Controller", description = "Manage item content.")
 @RestController
 public class ItemController {
